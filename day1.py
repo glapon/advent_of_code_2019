32559759d3e12a50caee66556a63d60f394e487a
Gary Lapon <gary@relht.com>
2019-12-03 12:14:15 -0500

    updated names on day 1

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
 input_file = open("input1.txt", "r")
 input = input_file.readlines()
-masses = [int(m) / 3 - 2 for m in input]
-print sum(masses)
+fuel = [int(mass) / 3 - 2 for mass in input]
+print sum(fuel)
